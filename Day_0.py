a8b54e0cf246d6e7d78a13640524f560d0a7680f
prashant <prashantchikhalkar@gmail.com>
2016-07-21 14:52:42 +0530

    Update Day_0.py

--- Day_0.py
+++ Day_0.py
@@ -1,1 +1,10 @@
+# https://www.hackerrank.com/challenges/30-hello-world
 
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+inputString = raw_input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print 'Hello, World.'
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+print (inputString)
